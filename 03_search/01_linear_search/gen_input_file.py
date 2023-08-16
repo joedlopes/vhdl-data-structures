2417318c8a0372716927199b07f60cd45f5ab9a1
joedlopes <joedlopes@gmail.com>
2023-08-16 23:52:02 +0200

    linear search

--- 03_search/01_linear_search/gen_input_file.py
+++ 03_search/01_linear_search/gen_input_file.py
@@ -1,6 +1,6 @@
 NUM_ROWS = 256
 with open("input_file.txt", "w") as f:
     for idx in range(NUM_ROWS):
-        idx = 255 - idx
+        idx = max(129, idx)
         # print(idx)
         f.write(f"{idx:08b}\n")
