8858364602fc3351132218a3c3c2be62d3f26b42
Joed <joedlopes@gmail.com>
2023-08-21 02:32:51 +0200

    binary search golden algorithm

--- 03_search/03_binary_search/README.md
+++ 03_search/03_binary_search/README.md
@@ -2,4 +2,85 @@
 
 This example implements a binary search algorithm.
 
+Given a Block RAM of 8bit address (0 to 255 values),
+and a target value, the designed circuit should return
+the closest index and value.
+
+The main condition for the binary search is that the values
+are already sorted.
+
+As follows a basic implementation of the binary search algorith:
+
+```python
+def binary_search(bram: List[int], 
+                  idx_left: int,
+                  idx_right: int,
+                  target: int,
+) -> Tuple[int, int]:
+    idx_middle: int
+
+    while idx_left != idx_right:
+        idx_middle = (idx_right - idx_left) // 2
+        
+        if target < bram[idx_middle]:
+            idx_right = idx_middle
+        elif target > bram[idx_middle]:
+            idx_left = idx_mid
+        else:
+            idx_left = idx_middle
+            idx_right = idx_middle
+
+    return idx_left, bram[idx_left]
+```
+
+From the following algorithm we can derive the following requirements:
+
+1. Only one address of the array can be read at the time using the Block RAM.
+   Therefore, to read the boundary values it is required two cycles in the
+   beginning, and another two cycles in the end.
+
+2. Integer division: the addresses must be divided to update boundaries.
+   Here, we can shift the address bits to right:
+   255 / 2 = 127   -->   0b11111111 >> 0b1 = 0b01111111
+   128 / 2 = 63    -->   0b01111111 >> 0b1 = 0b00111111
+   
+   Therefore, we do not need a "divisor" unit to operate here.
+   In VHDL we have the logical operator srl:
+   "11111111" srl 1
+
+
+Based on the previous requirements we can adapt the python code:
+
+```python
+def binary_search(bram: List[int], 
+                  idx_left: int,
+                  idx_right: int,
+                  target: int,
+) -> Tuple[int, int]:
+    idx_middle: int
+
+    while idx_left != idx_right:
+        idx_middle = (idx_right - idx_left) >> 1
+        idx_middle = (idx_right - idx_left) >> 1
+        
+        val_mid = bram[idx_middle]
+
+        if target < val_mid:
+            idx_right = idx_middle
+        elif target > val_mid:
+            idx_left = idx_mid
+        else:
+            idx_left = idx_middle
+            idx_right = idx_middle
+
+    return idx_left, bram[idx_left]
+```
+
+0  1   2   3   4   5   6   7
+0, 30, 20, 20, 20, 50, 60, 70
+
+t = 25
+L = 0
+R = 7
+M = (0 + 7) // 2 = 3
 
--- 03_search/03_binary_search/gen_input_file.py
+++ 03_search/03_binary_search/gen_input_file.py
@@ -1,11 +1,176 @@
-NUM_ROWS = 256
-with open("input_file.txt", "w") as f:
-    for idx in range(NUM_ROWS):
+import random
+from typing import List, Tuple
+import time
 
-        # if idx != 25:
-        #     idx = 10
-        # else:
-        #     idx = 200
-        # idx = min(79, idx)
-        # print(idx)
-        f.write(f"{idx:08b}\n")
+NUM_ROWS = 256
+
+arr = list()
+
+for idx in range(NUM_ROWS):
+    val = random.randint(0, 255)
+    # val = idx  # best case
+    # val = 255 - idx  # worst case
+    arr.append(val)
+
+arr = sorted(arr)
+print("Input Array:")
+
+with open("input_file.txt", "w") as f:
+    for val in arr:
+        f.write(f"{val:08b}\n")
+
+
+def linear_search(
+    bram: List[int],
+    idx_left: int,
+    idx_right: int,
+    target: int,
+) -> Tuple[int, int]:
+
+    # check target is in the range
+    if target <= bram[idx_left]:
+        return idx_left, bram[idx_left]
+    if target > bram[idx_right]:
+        return idx_right, bram[idx_right]
+
+    # equal
+    idx: int = idx_left
+    while idx < idx_right:
+        if bram[idx] == target:  # first equal
+            return idx, bram[idx]
+        # print(bram[idx], target, bram[idx + 1])
+        if bram[idx] < target and bram[idx + 1] > target:  # value between
+            if (target - bram[idx]) <= (bram[idx + 1] - target):
+                return idx, bram[idx]
+            else:
+                return idx + 1, bram[idx + 1]
+
+        idx += 1
+
+    return idx - 1, bram[idx - 1]
+
+
+def binary_search(
+    bram: List[int],
+    idx_left: int,
+    idx_right: int,
+    target: int,
+) -> Tuple[int, int]:
+    idx_middle: int = 0
+    cycles: int = 0
+
+    if target <= bram[0]:
+        print("LOW RANGE")
+        return 0, bram[0]
+    elif target > bram[len(bram)-1]:
+        print("HIGH RANGE")
+        return len(bram)-1, bram[len(bram)-1]
+
+    while idx_left < idx_right - 2:
+
+        idx_middle = (idx_left >> 1) + (idx_right >> 1)
+        val_mid = bram[idx_middle]
+
+        print(f"[{idx_left:02d}, {idx_right:02d}]", "idx_mid =", idx_middle,
+              "val =", val_mid, "target =", target)
+
+        time.sleep(0.01)
+
+        if target > val_mid:
+            idx_left = idx_middle
+            print("idx_left =", idx_left)
+
+        elif target < val_mid:
+            idx_right = idx_middle
+            print("idx_right =", idx_right)
+        else:
+            print(f"!FOUND[{idx_left:03d}, {idx_middle:03d}, {idx_right:03d}]")
+            idx_right = idx_middle
+
+    print("OUT")
+    # idx_middle = (idx_left >> 1) + (idx_right >> 1)
+    if idx_middle == idx_left:
+        idx_middle = idx_middle + 1
+    elif idx_middle == idx_right:
+        idx_middle = idx_middle - 1
+
+    print(f"!rEND[{idx_left:03d}, {idx_middle:03d}, {idx_right:03d}]")
+    print()
+
+    # val_left = bram[idx_left]
+    val_mid = bram[idx_middle]
+    # val_right = bram[idx_right]
+    
+    if target < val_mid:
+        idx_right = idx_middle
+    else:
+        idx_left = idx_middle
+
+    print(f"!xEND[{idx_left:03d}, {idx_middle:03d}, {idx_right:03d}]")
+
+    idx_min = idx_left
+    idx_max = idx_right
+    # idx_min = min(idx_left, min(idx_middle, idx_right))
+    # idx_max = max(idx_left, max(idx_middle, idx_right))
+
+    print("minmax", idx_min, idx_max)
+    val_min = bram[idx_min]
+    val_max = bram[idx_max]
+    print("val", val_min, val_max)
+
+    if val_min == target:
+        idx_left = idx_min
+    elif val_max == target:
+        idx_left = idx_max
+    else:
+        A = target - val_min
+        B = val_max - target
+
+        if A <= B:
+            idx_left = idx_min
+        else:
+            idx_left = idx_max
+
+    return idx_left, bram[idx_left]
+
+
+target = random.randint(0, 255)
+
+if True:
+    # target = 63
+    # target = 252
+    # arr = [8, 9, 26, 62, 62, 111, 111, 111, 111, 111, 111, 111, 189, 189, 252, 252]
+    # arr = [3, 6, 32, 34, 41, 62, 64, 94, 103, 110, 111, 184, 191, 207, 214, 253]
+    # arr = [18, 22, 27, 42, 42, 43, 78, 135, 160, 176, 182, 197, 216, 216, 237, 243]
+    # arr = [17, 24, 29, 43, 91, 107, 130, 134, 139, 143, 144, 153, 153, 153, 220, 240]
+    # arr = [7, 8, 13, 13, 39, 68, 78, 94, 99, 104, 105, 203, 217, 223, 232, 238]
+    # arr = [8, 9, 26, 62, 62, 111, 111, 111, 111, 111, 111, 111, 189, 189, 221, 252]
+    # arr = [15, 28, 31, 51, 63, 82, 90, 100, 116, 129, 191, 191, 197, 206, 232, 232]
+    # target = 28
+    # arr = [13, 21, 24, 32, 60, 71, 91, 129, 154, 184, 187, 213, 213, 215, 231, 248]
+    print(", ".join([f"{x:03d}" for x in range(len(arr))]))
+    print(", ".join([f"{x:03d}" for x in arr]))
+else:
+    print(arr)
+
+
+# target = 3000
+print("Target Value:", target)
+
+idx_gt, val_gt = linear_search(arr, 0, len(arr)-1, target)
+
+print("Expected Output:", )
+idx_res = 0
+idx_res, val_res = binary_search(arr, 0, len(arr)-1, target)
+
+print(f"> val gt:  {arr[idx_gt]:03d}, idx: {idx_gt:03d}")
+print(f"> val res: {val_res:03d}, idx: {idx_res:03d}")
+
+if idx_res == idx_gt:
+    print("PASSED!")
+else:
+    print()
+    print("FAIL!FAIL!FAIL!FAIL!FAIL!FAIL!FAIL!FAIL!FAIL!FAIL!FAIL!")
+    print()
+
+print("-vhdl-")
